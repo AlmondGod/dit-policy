397ad758230501fcb7a961dbcc20c63598fb1adc
AlmondGod <anandmajmudar1@gmail.com>
2024-12-23 20:11:30 -0500

    obs update 3.0

--- observations.py
+++ observations.py
@@ -1,5 +1,6 @@
 import numpy as np
 import copy
+import cv2
 
 class DummyImage:
     def __init__(self):
@@ -15,47 +16,39 @@ class DummyImage:
 
 class DummyObs:
     def __init__(self):
-        self.state = np.zeros(7, dtype=np.float32)
-        self._image = DummyImage()
-        self.prev = None
-        self._init_obs()
-    
-    def _init_obs(self):
-        """Initialize the observation dictionary"""
         self._obs = {
-            'state': self.state,
+            'state': np.zeros(7, dtype=np.float32),
             'enc_cam_0': self._create_encoded_image()
         }
+        self.prev = None
     
     def _create_encoded_image(self):
-        """Create a checkerboard pattern and encode it"""
+        # Create a checkerboard pattern
         x = np.arange(256)[:, None]
         y = np.arange(256)[None, :]
         checker = ((x + y) % 32 < 16).astype(np.uint8) * 255
         img = np.stack([checker] * 3, axis=-1)
-        return img.tobytes()
+        # Convert to BGR for OpenCV
+        bgr_img = img[:, :, ::-1]
+        _, encoded = cv2.imencode(".jpg", bgr_img)
+        return encoded.tobytes()
     
-    def __getstate__(self):
-        """Called when pickling"""
-        return {
-            'state': self.state,
-            'prev': self.prev,
-            '_image': self._image
-        }
-    
-    def __setstate__(self, state_dict):
-        """Called when unpickling"""
-        self.state = state_dict['state']
-        self.prev = state_dict['prev']
-        self._image = state_dict['_image']
-        self._init_obs()
+    @property
+    def state(self):
+        return self._obs['state']
     
     def image(self, cam_idx):
-        """Return RGB image of shape (H, W, 3)"""
-        return self._image.image(cam_idx)
+        encoded_image_np = np.frombuffer(self._obs[f'enc_cam_{cam_idx}'], dtype=np.uint8)
+        bgr_image = cv2.imdecode(encoded_image_np, cv2.IMREAD_COLOR)
+        rgb_image = bgr_image[:, :, ::-1]
+        return rgb_image
     
+    # Dictionary interface methods
     def keys(self):
         return self._obs.keys()
+    
+    def items(self):
+        return self._obs.items()
     
     def __getitem__(self, key):
         return self._obs[key]
