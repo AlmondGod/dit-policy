f4e49e12fae694f24769ca918b9e7c521978826d
AlmondGod <anandmajmudar1@gmail.com>
2024-12-25 19:44:15 -0500

    10 steps

--- diffusion_visualization.py
+++ diffusion_visualization.py
@@ -63,8 +63,8 @@ class DiffusionVisualizer:
                 n_cams=1,  # From task.n_cams in config
                 use_obs="add_token",  # From config
                 ac_dim=6,  # From task.ac_dim in config
-                ac_chunk=100,  # From config
-                train_diffusion_steps=100,  # From config
+                ac_chunk=10,  # From config
+                train_diffusion_steps=10,  # From config
                 eval_diffusion_steps=8,  # From config
                 imgs_per_cam=1,  # Default value
                 dropout=0.1,  # From config
