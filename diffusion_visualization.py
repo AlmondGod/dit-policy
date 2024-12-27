1194e89351e32b43f2f876218b18ce1814f0148a
AlmondGod <anandmajmudar1@gmail.com>
2024-12-27 17:29:27 -0500

    much larger box

--- diffusion_visualization.py
+++ diffusion_visualization.py
@@ -257,8 +257,8 @@ def main():
             substeps=20
         ),
         mpm_options=gs.options.MPMOptions(
-            lower_bound=(-0.35, -0.35, -0.35),  # Reduced from -2.0
-            upper_bound=(0.35, 0.35, 0.35),    # Reduced from 2.0
+            lower_bound=(-0.8, -0.8, -0.8),  # Reduced from -2.0
+            upper_bound=(0.8, 0.8, 0.8),    # Reduced from 2.0
             grid_density=10,  # Added to reduce memory usage
         ),
         # viewer_options=gs.options.ViewerOptions(
@@ -283,7 +283,7 @@ def main():
         material=gs.materials.MPM.Liquid(),
         morph=gs.morphs.Box(
             pos=(0.0, 0.0, 0.0),
-            size=(0.15, 0.15, 0.15)
+            size=(0.35, 0.35, 0.35)
         ),
         surface=gs.surfaces.Default(
             color=(0.2, 0.6, 1.0, 1.0),  # Default color
